10722bdb97a12b162148e62441791bbe86f0d1dc
Kelias1 <kelsito24@gmail.com>
2024-04-25 20:01:03 +0300

    second commit

--- src/js/app.js
+++ src/js/app.js
@@ -1,14 +1,14 @@
 document.addEventListener("DOMContentLoaded", () => {
-  const parentDiv = document.querySelector('.wrapper');
+  const parentDiv = document.querySelector(".wrapper");
   const img = document.images[0];
   img.parentElement.removeChild(img);
 
   for (let i = 0; i < 16; i += 1) {
-    const div = document.createElement('div');
+    const div = document.createElement("div");
     parentDiv.appendChild(div);
   }
 
-  const parentChildrens = parentDiv.children
+  const parentChildrens = parentDiv.children;
 
   function genPosition() {
     return Math.floor(Math.random() * (parentChildrens.length - 1));
@@ -26,5 +26,7 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   const timerId = setInterval(setActive, 1000);
-  setTimeout(() => { clearInterval(timerId); }, 20000);
-})
+  setTimeout(() => {
+    clearInterval(timerId);
+  }, 20000);
+});
