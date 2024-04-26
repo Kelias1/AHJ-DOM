1fee0210c743ef819cb4bf580feca1990784ad62
Kelias1 <kelsito24@gmail.com>
2024-04-26 20:06:42 +0300

    V.2

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,12 @@
+import img from "../img/goblin.png";
+
 document.addEventListener("DOMContentLoaded", () => {
   const parentDiv = document.querySelector(".wrapper");
-  const img = document.images[0];
-  img.parentElement.removeChild(img);
+  const image = document.createElement("img");
+  image.src = img;
+  // console.log(image);
+
+  image.parentElement.removeChild(img);
 
   for (let i = 0; i < 16; i += 1) {
     const div = document.createElement("div");
@@ -15,14 +20,15 @@ document.addEventListener("DOMContentLoaded", () => {
   }
 
   function setActive() {
-    if (document.images[0]) {
-      const activeDiv = document.images[0].parentElement;
-      activeDiv.removeChild(img);
+    if (image) {
+      const activeDiv = image.parentElement;
+      // console.log(image);
+      activeDiv.removeChild(image);
     }
 
     const childDivs = parentDiv.children;
     const index = genPosition();
-    childDivs[index].appendChild(img);
+    childDivs[index].appendChild(image);
   }
 
   const timerId = setInterval(setActive, 1000);
@@ -30,3 +36,40 @@ document.addEventListener("DOMContentLoaded", () => {
     clearInterval(timerId);
   }, 20000);
 });
+
+
+// ====================================
+// document.addEventListener("DOMContentLoaded", () => {
+//   const parentDiv = document.querySelector(".wrapper");
+//   const img = document.images[0];
+//   img.parentElement.removeChild(img);
+
+//   for (let i = 0; i < 16; i += 1) {
+//     const div = document.createElement("div");
+//     parentDiv.appendChild(div);
+//   }
+
+//   const parentChildrens = parentDiv.children;
+
+//   function genPosition() {
+//     return Math.floor(Math.random() * (parentChildrens.length - 1));
+//   }
+
+//   function setActive() {
+//     if (document.images[0]) {
+//       const activeDiv = document.images[0].parentElement;
+//       activeDiv.removeChild(img);
+//     }
+
+//     const childDivs = parentDiv.children;
+//     const index = genPosition();
+//     childDivs[index].appendChild(img);
+//   }
+
+//   const timerId = setInterval(setActive, 1000);
+//   setTimeout(() => {
+//     clearInterval(timerId);
+//   }, 20000);
+// });
+
+// =================================
