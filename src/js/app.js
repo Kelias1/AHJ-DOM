942f1af5cd127b8d4d4d0e47ca01c9b0657ba47d
Kelias1 <kelsito24@gmail.com>
2024-04-29 07:57:26 +0300

    V 9

--- src/js/app.js
+++ src/js/app.js
@@ -1,6 +1,5 @@
 import image from "../img/goblin.png";
 
-document.addEventListener("DOMContentLoaded", () => {
   const boxes = document.querySelector(".box");
   const goblin = document.createElement("img");
   goblin.src = image;
@@ -21,8 +20,38 @@ document.addEventListener("DOMContentLoaded", () => {
     const newBox = getBox();
     if (newBox !== currentBox) {
       currentBox = newBox;
-      currentBox.appendChild(goblin);
+      currentBox.append(goblin);
     }
   }
   setInterval(moveGoblin, 1000);
-});
+
+
+
+// import image from "../img/goblin.png";
+
+// document.addEventListener("DOMContentLoaded", () => {
+//   const boxes = document.querySelector(".box");
+//   const goblin = document.createElement("img");
+//   goblin.src = image;
+
+//   for (let i = 0; i < 16; i++) {
+//     let div = document.createElement("div");
+//     boxes.appendChild(div);
+//   }
+  
+//   let currentBox = null;
+
+//   function getBox() {
+//     const i = Math.floor(Math.random() * boxes.length);
+//     return boxes[i];
+//   }
+
+//   function moveGoblin() {
+//     const newBox = getBox();
+//     if (newBox !== currentBox) {
+//       currentBox = newBox;
+//       currentBox.appendChild(goblin);
+//     }
+//   }
+//   setInterval(moveGoblin, 1000);
+// });
