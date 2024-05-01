c1ef9226e21b8364a7a09e58d6f3168f82999af8
Kelias1 <kelsito24@gmail.com>
2024-05-01 11:39:58 +0300

    V 12

--- src/js/app.js
+++ src/js/app.js
@@ -3,6 +3,8 @@ import image from "../img/goblin.png";
 document.addEventListener("DOMContentLoaded", () => {
   const box = document.querySelector(".box");
   const board = [];
+  const goblin = document.createElement("img");
+  goblin.src = image;
 
   for (let i = 0; i < 16; i++) {
     let div = document.createElement("div");
@@ -16,9 +18,6 @@ document.addEventListener("DOMContentLoaded", () => {
     board[currentBox].innerHTML = '';
 
     currentBox = Math.floor(Math.random() * board.length);
-
-    const goblin = document.createElement("img");
-    goblin.src = image;
     board[currentBox].append(goblin);
   }
   setInterval(moveGoblin, 1000);
